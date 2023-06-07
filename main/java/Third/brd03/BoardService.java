0a0f482f2600aa9424cb3553d9f4da09876c6f6f
myeong <audelahd@gmail.com>
2023-06-07 12:20:36 +0900

    글 열람시 글 번호, 작성자 아이디, 제목, 내용, 이미지, 등록일자가 뜨도록 작성

---
+++ main/java/Third/brd03/BoardService.java
@@ -0,0 +1,28 @@
+package Third.brd03;
+
+import java.util.List;
+
+public class BoardService {
+	BoardDAO boardDAO;
+	
+	public BoardService() {
+		boardDAO = new BoardDAO();
+	}
+	
+	public int addArticle(ArticleVO article) {
+		return boardDAO.insertNewArticle(article);
+	}
+	
+	
+	public List<ArticleVO> listArticles(){
+		List<ArticleVO> articlesList = boardDAO.selectAllArticles();
+		return articlesList;
+	}
+	
+	public ArticleVO viewArticle(int articleNO) {
+		ArticleVO article = null;
+		article = boardDAO.selectArticle(articleNO);
+		return article;
+	}
+
+}
